4bdc894829862114585503a385caa7a28532b260
Dannyzlh <Danielzlh62@gmail.com>
2016-05-24 09:25:34 +0800

    modify server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const http404 = require('./utils/404')();
 // const http403 = require('./utils/403')();
 
 // Configuration
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use( function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -27,6 +27,7 @@ app.use( function(req, res, next) {
 });
 
 //  Connect all our routes to our application
+
 app.use('/', routes);
 app.use('/*', function(req,res){ http404.notFoundMiddleware(req,res); });
 
@@ -36,5 +37,7 @@ app.listen(port,function(){
 		'\n__dirname = ' + __dirname + 
 		'\nprocess.cwd = ' + process.cwd());
 });
+
+// Export config
 
 module.exports = {'jwt':jwt,'secret':secret};
