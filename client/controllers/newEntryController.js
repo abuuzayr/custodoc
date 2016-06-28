380dad5420508dc77a44eab29b9107f505d1395d
Gilbert <chuakianchiu@gmail.com>
2016-06-28 18:18:30 +0800

    add comment

--- client/controllers/newEntryController.js
+++ client/controllers/newEntryController.js
@@ -39,8 +39,9 @@ angular
 		{type: "email", name: "Email", label: "Email", data:""},
 		{type: "text", name: "Country", label: "Country", data:"Singapore"},
 		{type: "text", name: "State", label: "State", data:"Tampines"},
+		{type: "text", name: "Max", label: "Max", data:""},
 		{type: "text", name: "Address", label: "Address", data:"Blk 138"},
-		{type: "dropdown", name: "Gender", label: "Gender", options: ["Male", "Female"], data:""},
+		{type: "dropdown", name: "Gender", label: "Gender", options: ["Male", "Female"], data:"Male"},
 		{type: "image", name: "Picture", label: "Picture", data:""},
 		{type: "checkbox", name: "Are you Human", label: "Are you Human?", data:"false"}  // true/false questions
 	    ]
