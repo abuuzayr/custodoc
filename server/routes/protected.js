991e2bc56a3a23e883d6ab7ff5b0c4e8bee6b488
Dannyzlh <Danielzlh62@gmail.com>
2016-07-04 14:17:28 +0800

    added authentication module

--- server/routes/protected.js
+++ server/routes/protected.js
@@ -2,6 +2,7 @@ var protectedRoutes = require('express').Router();
 //import modules
 var auth = require('../utils/403.js')();
 var http404 = require('../utils/404.js')();
+var http403 = require('../utils/403.js')();
 //add routes
 var autofillRoutes = require('./protected/autofill.js');
 var entryrecordsRoutes = require('./protected/entryRecords.js');
@@ -9,7 +10,11 @@ var entryRoutes = require('./protected/entryRouter.js')
 var formRoutes = require('./protected/formsRouter.js');
 var groupRoutes = require('./protected/groupsRouter.js');
 
+
 //PROTECTED ROUTES - REQUIRES TOKEN_AUTH & ACL CHECKINGS
+protectedRoutes.use('*',http403.authenticateToken);
+//protectedRoutes.use('*',http403.decodeAccessInfo);
+//PATH
 protectedRoutes.use('/autofill', autofillRoutes);
 protectedRoutes.use('/entryrecords', entryrecordsRoutes);
 protectedRoutes.use('/entries',entryRoutes);
