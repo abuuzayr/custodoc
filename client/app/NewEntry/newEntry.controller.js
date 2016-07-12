d0a63ddee06d5e629e9f49eeec3bd28be89c217a
Gilbert <chuakianchiu@gmail.com>
2016-07-12 17:56:39 +0800

    remove signature functions to fix newEntry

--- client/app/NewEntry/newEntry.controller.js
+++ client/app/NewEntry/newEntry.controller.js
@@ -31,7 +31,7 @@ angular
 	vm.image = null;
 	vm.signature = null;
 	
-	// vm.gotSignature = false;
+	vm.gotSignature = false;
 
 	vm.groupName = $stateParams.groupName;
 	
@@ -429,61 +429,62 @@ angular
 
 	/****************** SIGNATURE PAD VARIABLES **********************/
 
-	
-	vm.wrapper = angular.element(document.getElementById('signature-field-div'));
-	vm.dialog = angular.element(vm.wrapper.find('dialog'))[0];
-	vm.canvas = angular.element(vm.wrapper.find('canvas'))[0];
-	console.log('wats dialog ' + vm.dialog);
-	console.log('wats canvas ' + vm.canvas);
-	vm.signaturePad = new SignaturePad(vm.canvas);
-	
+	if (vm.gotSignature) {
+		vm.wrapper = angular.element(document.getElementById('signature-field-div'));
+		vm.dialog = angular.element(vm.wrapper.find('dialog'))[0];
+		vm.canvas = angular.element(vm.wrapper.find('canvas'))[0];
+		console.log('wats dialog ' + vm.dialog);
+		console.log('wats canvas ' + vm.canvas);
+		vm.signaturePad = new SignaturePad(vm.canvas);
+	}
 
 	/*****************************************************************/
 
 
 	/*********************** SIGNATURE PAD FUNCTIONS *************************/
 
+	if (vm.gotSignature) {
 
-	vm.openModal = function() {
-		vm.dialog.showModal();	
-	}
-	
-	vm.closeModal = function() {
-		vm.dialog.close();	
-	}
+		vm.openModal = function() {
+			vm.dialog.showModal();	
+		}
+		
+		vm.closeModal = function() {
+			vm.dialog.close();	
+		}
 
-	vm.clear = function() {
-		vm.signaturePad.clear();
-	};
+		vm.clear = function() {
+			vm.signaturePad.clear();
+		};
 
-	vm.save = function() {
-		console.log('haha');
-		if (vm.signaturePad.isEmpty()) {
-    		var msg = "Please provide signature first.";
-    		showSnackbar(msg);
-		 	} else {
-		 		var dataURL = vm.signaturePad.toDataURL('image/png',1);
-		 		//Open image in new window
-			//window.open(dataURL);
-			//..or
-			//Extract as base64 encoded
-			var data = dataURL.substr(dataURL.indexOf('base64') + 7)
-			vm.signature = data;
-			console.log(data);
-			//TODO: include in your json object
+		vm.save = function() {
+			console.log('haha');
+			if (vm.signaturePad.isEmpty()) {
+	    		var msg = "Please provide signature first.";
+	    		showSnackbar(msg);
+			 	} else {
+			 		var dataURL = vm.signaturePad.toDataURL('image/png',1);
+			 		//Open image in new window
+				//window.open(dataURL);
+				//..or
+				//Extract as base64 encoded
+				var data = dataURL.substr(dataURL.indexOf('base64') + 7)
+				vm.signature = data;
+				console.log(data);
+				//TODO: include in your json object
+			}
+		}
+
+		function showSnackbar(msg) {
+			var msgSnackbar = {
+				message: msg,
+				timeout: 5000
+			}
+			var snackbarContainer = document.querySelector('#snackbar-div');
+			console.log(snackbarContainer);
+			snackbarContainer.MaterialSnackbar.showSnackbar(msgSnackbar);
 		}
 	}
-
-	function showSnackbar(msg) {
-		var msgSnackbar = {
-			message: msg,
-			timeout: 5000
-		}
-		var snackbarContainer = document.querySelector('#snackbar-div');
-		console.log(snackbarContainer);
-		snackbarContainer.MaterialSnackbar.showSnackbar(msgSnackbar);
-	}
-
 
 	/*************************************************************************/
 
