89798bfc2c8026e34de8533b879a574483c0d064
Dannyzlh <Danielzlh62@gmail.com>
2016-07-15 18:46:20 +0800

    add auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ var auth = require('../utils/403.js')();
 var http404 = require('../utils/404.js')();
 var http403 = require('../utils/403.js')();
 
-authRoutes.use('/',http403,authenticateToken,function(req,res){
+authRoutes.use('/',http403.authenticateToken,function(req,res){
 	res.sendStatus(200);
 });
 
