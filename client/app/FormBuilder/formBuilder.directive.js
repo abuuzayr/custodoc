ef634534d44ae9ad3338cc420058cee251900037
Dannyzlh <Danielzlh62@gmail.com>
2016-07-22 18:12:26 +0800

    add directive  for ondrop and ondragover

--- client/app/FormBuilder/formBuilder.directive.js
+++ client/app/FormBuilder/formBuilder.directive.js
@@ -1,11 +1,33 @@
-angular.module('app.formBuilder')
-.directive('myOnDragStart',myOnDragStart);
+angular.module('app.formBuilder',[])
+.directive('myOnDragStart',myOnDragStart)
+.directive('myOnDragOver',myOnDragOver)
+.directive('myOnDrop',myOnDrop);
 
 function myOnDragStart() {
 	return {
 		restrict: 'A',
 		link: function(scope, element, attrs){
 			var onDragStartHandler = $scope.$eval(attrs.myOnDragStart);
+			element.bind('ondragstart',onDragStartHandler);
+		}
+	};
+}
+
+function myOnDragOver() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs){
+			var onDragStartHandler = $scope.$eval(attrs.myOnDragOver);
+			element.bind('ondragstart',onDragStartHandler);
+		}
+	};
+}
+
+function myOnDrop() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs){
+			var onDragStartHandler = $scope.$eval(attrs.myOnDrop);
 			element.bind('ondragstart',onDragStartHandler);
 		}
 	};
