1518fcbbfb069bb5b81ee775134490911331ade2
Angie <angietio94@gmail.com>
2016-07-25 10:38:45 +0800

    Add scope and transclude to top nav directive

--- client/app/Shared/topNav/topNav.directive.js
+++ client/app/Shared/topNav/topNav.directive.js
@@ -7,6 +7,10 @@
     function topNavBar() {
         return {
             restrict: 'EA',
+            scope: {
+                userInfo : '='
+            },
+            transclude: true,
             controller: 'topNavCtrl',
             controllerAs: 'vm',
             templateUrl: 'app/Shared/topNav/topNav.template.html',
