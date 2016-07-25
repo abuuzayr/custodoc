b401ddd3e52012b7ac98106a567d579dc0d69c85
Angie <angietio94@gmail.com>
2016-07-25 10:16:58 +0800

    IIFE top nav directive

--- client/app/Shared/topNav/topNav.controller.js
+++ client/app/Shared/topNav/topNav.controller.js
@@ -1,48 +1,40 @@
-angular
-    .module('app.shared')
-    .controller('topNavCtrl', topNavCtrl);
+(function () {
+    "use strict";
+    angular
+        .module('app.shared')
+        .controller('topNavCtrl', topNavCtrl);
 
-topNavCtrl.$inject = ['authServices', '$scope', '$interval'];
+    topNavCtrl.$inject = ['authServices', '$scope', '$interval'];
 
-function topNavCtrl(authServices, $scope, $interval) {
-    // $scope.$on('$viewContentLoaded', function () {
-    //     $timeout(function () {
-    //         viewContentLoaded.resolve();
-    //     }, 0);
-    // });
-    // viewContentLoaded.promise.then(function () {
-    //     $timeout(function () {
-    //         componentHandler.upgradeDom();
-    //     }, 0);
-    // });
+    function topNavCtrl(authServices, $scope, $interval) {
+        var vm = this;
+        vm.openNav = openNav;
+        vm.closeNav = closeNav;
+        vm.logout = logout;
 
-    var vm = this;
-    vm.openNav = openNav;
-    vm.closeNav = closeNav;
-    vm.logout = logout;
-
-    vm.date = new Date();
-
-    $interval(function () {
         vm.date = new Date();
-    }, 1000);
 
-    vm.username = authServices.getUserInfo().username;
-    vm.userType = authServices.getUserInfo().usertype;
+        $interval(function () {
+            vm.date = new Date();
+        }, 1000);
 
-    function logout() {
-        authServices.logout();
+        vm.username = authServices.getUserInfo().username;
+        vm.userType = authServices.getUserInfo().usertype;
+
+        function logout() {
+            authServices.logout();
+        }
+
+        /* Open the sidenav */
+        function openNav() {
+            console.log('OPEN SESAME');
+            document.getElementById("mySidenav").style.width = "100%";
+        }
+
+        /* Close/hide the sidenav */
+        function closeNav() {
+            console.log('CLOSEEE SESAME');
+            document.getElementById("mySidenav").style.width = "0";
+        }
     }
-
-    /* Open the sidenav */
-    function openNav() {
-        console.log('OPEN SESAME');
-        document.getElementById("mySidenav").style.width = "100%";
-    }
-
-    /* Close/hide the sidenav */
-    function closeNav() {
-        console.log('CLOSEEE SESAME');
-        document.getElementById("mySidenav").style.width = "0";
-    }
-}
+})();
--- client/app/Shared/topNav/topNav.directive.js
+++ client/app/Shared/topNav/topNav.directive.js
@@ -1,15 +1,15 @@
-angular
-    .module('app.shared')
-    .directive('topNavBar', topNavBar);
+(function () {
+    "use strict";
+    angular
+        .module('app.shared')
+        .directive('topNavBar', topNavBar);
 
-function topNavBar() {
-    return {
-        restrict: 'EA',
-        controller: 'topNavCtrl',
-        controllerAs: 'vm',
-        templateUrl: 'app/Shared/topNav/topNav.template.html',
-        link:function(){
-            console.log('linking');
-        }
-    };
-}    
+    function topNavBar() {
+        return {
+            restrict: 'EA',
+            controller: 'topNavCtrl',
+            controllerAs: 'vm',
+            templateUrl: 'app/Shared/topNav/topNav.template.html',
+        };
+    }
+})();
