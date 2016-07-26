df643f8519e0b03149ac4348f2a82216f0ae8d98
Dannyzlh <Danielzlh62@gmail.com>
2016-07-26 14:28:18 +0800

    refractor form route backend code

--- server/routes/protected/formsRouter.js
+++ server/routes/protected/formsRouter.js
@@ -135,26 +135,31 @@ formsRouter.route('/duplicate')
                     if (!item) {
                         res.send("Cannot find");
                     } else {
-                        coll.findOne({ formName: duplicateName, groupName: duplicateTo }, function(err, result) {
-                            if (result) {
-                                res.send('Existed');
-                            } else {
-                                var newItem = item;
-                                delete newItem._id;
-                                newItem.groupName = duplicateTo;
-                                newItem.formName = duplicateName;
-                                newItem.isImportant = 'Normal';
-                                newItem.creator = req.decoded.username;
-                                newItem.creationDate = new Date();
-                                newItem.lastModified = new Date();
-                                newItem.lastModifiedBy = req.decoded.username;
-                                coll.insert(newItem, function(err, result) {
-                                    assert.equal(null, err);
-                                    res.send("Duplicated:" + formName);
-                                    console.log("Duplicated the form");
-                                });
-                            }
-                        });
+                        db.collection("forms").find({
+                                formName: duplicateName,
+                                groupName: duplicateTo
+                            })
+                            .limit(1)
+                            .next(function(err, item) {
+                                if (result) {
+                                    res.send('Existed');
+                                } else {
+                                    var newItem = item;
+                                    delete newItem._id;
+                                    newItem.groupName = duplicateTo;
+                                    newItem.formName = duplicateName;
+                                    newItem.isImportant = 'Normal';
+                                    newItem.creator = req.decoded.username;
+                                    newItem.creationDate = new Date();
+                                    newItem.lastModified = new Date();
+                                    newItem.lastModifiedBy = req.decoded.username;
+                                    coll.insert(newItem, function(err, result) {
+                                        assert.equal(null, err);
+                                        res.send("Duplicated:" + formName);
+                                        console.log("Duplicated the form");
+                                    });
+                                }
+                            });
                     }
                 });
         });
@@ -162,19 +167,19 @@ formsRouter.route('/duplicate')
 
 formsRouter.route('/importance')
     .put(function(req, res, next) {
-        var db = require('../../server.js').db;
-        var coll = db.collection("forms");
-        coll.updateOne({
-            'formName': req.body.formName,
-            'groupName': req.body.groupName,
-        }, {
-            $set: {
-                "isImportant": req.body.importance
-            }
-        }, function(err, results) {
-            assert.equal(err, null);
-            console.log(results);
-            res.send(results);
+        connection.Do(function(db) {
+            db.collection("forms").updateOne({
+                'formName': req.body.formName,
+                'groupName': req.body.groupName,
+            }, {
+                $set: {
+                    "isImportant": req.body.importance
+                }
+            }, function(err, results) {
+                assert.equal(err, null);
+                console.log(results);
+                res.send(results);
+            });
         });
     });
 
@@ -182,25 +187,30 @@ formsRouter.route('/:groupName/:formName')
     .get(function(req, res, next) {
         var groupName = req.params.groupName;
         var formName = req.params.formName;
-        var db = require('../../server.js').db;
-        var coll = db.collection("forms");
-        coll.findOne({ formName: formName, groupName: groupName }, function(err, item) {
-            assert.equal(null, err);
-            if (item) {
-                res.send(item);
-            } else {
-                res.status(404).send({ error: 'Cannot find the form' });
-            }
+        connection.Do(function(db) {
+            db.collection("forms").find({
+                    formName: formName,
+                    groupName: groupName
+                })
+                .limit(1)
+                .next(function(err, item) {
+                    assert.equal(null, err);
+                    if (item) {
+                        res.send(item);
+                    } else {
+                        res.status(404).send({ error: 'Cannot find the form' });
+                    }
+                });
         });
     })
     .delete(function(req, res, next) {
         var groupName = req.params.groupName;
         var formName = req.params.formName;
-        var db = require('../../server.js').db;
-        var coll = db.collection("forms");
-        coll.deleteOne({ "groupName": groupName, "formName": formName }, function(err, result) {
-            assert.equal(null, err);
-            res.send(groupName + '/' + formName);
+        connection.Do(function(db) {
+            db.collection("forms").deleteOne({ "groupName": groupName, "formName": formName }, function(err, result) {
+                assert.equal(null, err);
+                res.send(groupName + '/' + formName);
+            });
         });
     });
 
