7f056ee65598b40db74d0e29b6d907c62f15aba2
Dannyzlh <Danielzlh62@gmail.com>
2016-07-26 15:47:04 +0800

    modify app.js

--- client/app/app.js
+++ client/app/app.js
@@ -2,13 +2,13 @@
     'use strict';
 
     angular
-        .module('app.core', ['ui.router', 'ngTouch', 'app.formBuilder', 'app.formMgmt', 'app.autofill', 'app.newEntry', 'app.shared','app.entryMgmt'])
+        .module('app.core', ['ui.router', 'ui.grid', 'ui.grid.autoResize', 'ui.grid.pagination', 'ui.grid.resizeColumns', 'ui.grid.moveColumns', 'ngTouch', 'ui.grid.selection', 'app.formBuilder', 'app.formMgmt', 'app.autofill', 'app.newEntry', 'app.shared','app.entryMgmt'])
         .config(['$urlRouterProvider', '$stateProvider', '$locationProvider', '$compileProvider', function($urlRouterProvider, $stateProvider, $locationProvider, $compileProvider) {
             // Set AngularJS production mode
             $compileProvider.debugInfoEnabled(false);
             // Set HTML5 mode
             $locationProvider.html5Mode(true);
-            $urlRouterProvider.otherwise('/');
+            $urlRouterProvider.otherwise('/forms');
             $stateProvider
                 .state('login', {
                     url: '/login',
