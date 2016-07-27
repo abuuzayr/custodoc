a7f0aa36a94d903daa7918daa2914e927ee60e01
Angie <angietio94@gmail.com>
2016-07-27 16:42:53 +0800

    Undo replace top nav directive

--- client/app/Shared/topNav/topNav.directive.js
+++ client/app/Shared/topNav/topNav.directive.js
@@ -7,7 +7,6 @@
     function topNavBar() {
         return {
             restrict: 'EA',
-            replace: true,
             controller: 'topNavCtrl',
             controllerAs: 'vm',
             templateUrl: 'app/Shared/topNav/topNav.html'
