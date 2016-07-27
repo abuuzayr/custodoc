d80f30fabed3345f4591bf6e8438ec999437c017
Dannyzlh <Danielzlh62@gmail.com>
2016-07-27 16:16:02 +0800

    change query with storage routes

--- server/routes/storage.js
+++ server/routes/storage.js
@@ -4,6 +4,8 @@ var sendError = require('../utils/errorHandler.js');
 var connection = require('../utils/connection.js')();
 var http404 = require('../utils/404.js')();
 var http403 = require('../utils/403.js')();
+var ObjectID = require('mongodb').ObjectID;
+
 
 storageRoutes.get('/',http403.decodeSessionCookie,getFormStorage,getEntryStorage,getAutofillStorage,function(req,res){
 	return res.status(200).send({
@@ -15,7 +17,7 @@ storageRoutes.get('/',http403.decodeSessionCookie,getFormStorage,getEntryStorage
 
 function getFormStorage(req,res,next){
 	connection.Do(function(db){
-		db.collection('forms').find({companyId: req.decoded.companyId}).toArray().then(function(docs){
+		db.collection('forms').find({companyId: ObjectID(req.decoded.companyId)}).toArray().then(function(docs){
 			req.formStorage = 0;
 			if(!docs || docs.length === 0)
 				next();
@@ -30,7 +32,7 @@ function getFormStorage(req,res,next){
 }
 function getEntryStorage(req,res,next){
 	connection.Do(function(db){
-		db.collection('entries').find({companyId: req.decoded.companyId}).toArray().then(function(docs){
+		db.collection('entries').find({companyId: ObjectID(req.decoded.companyId)}).toArray().then(function(docs){
 			req.entryStorage = 0;
 			if(!docs || docs.length === 0)
 				next();
@@ -45,7 +47,7 @@ function getEntryStorage(req,res,next){
 }
 function getAutofillStorage(req,res,next){
 	connection.Do(function(db){
-		db.collection('autofill').find({companyId: req.decoded.companyId}).toArray().then(function(docs){
+		db.collection('autofill').find({companyId: ObjectID(req.decoded.companyId)}).toArray().then(function(docs){
 			req.autofillStorage = 0;
 			if(!docs || docs.length === 0)
 				next();
