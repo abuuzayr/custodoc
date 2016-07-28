9adc474b694e14551837501d5e0c7d6dbbb8450f
Gilbert <chuakianchiu@gmail.com>
2016-07-28 17:14:56 +0800

    add slugify code

--- client/app/EntryMgmt/entryMgmt.controller.js
+++ client/app/EntryMgmt/entryMgmt.controller.js
@@ -53,6 +53,15 @@ angular.module('app.entryMgmt')
 
 		/************************************************************/
 
+		function slugify(text) {
+		  	return text.toString().toLowerCase()
+		    .replace(/\s+/g, '_')           // Replace spaces with -
+		    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+		    .replace(/\-\-+/g, '_')         // Replace multiple - with single -
+		    .replace(/^-+/, '')             // Trim - from start of text
+		    .replace(/-+$/, '');            // Trim - from end of text
+		}
+
 		function getData(){
 			return entryMgmtServices.getData()
 			.then(SuccessCallback)
