ad7a7fbc48289431f015ad11edce2e75e49c3e74
Angie <angietio94@gmail.com>
2016-07-28 11:12:00 +0800

    Revert redirect from login changes

--- client/app/app.auth.js
+++ client/app/app.auth.js
@@ -15,13 +15,5 @@
                 $state.go('login');
             }
         });
-        // $rootScope.$on('$stateChangeStart', function (event, toState) {
-        //     if (toState.name === 'login') {
-        //         if (authServices.getToken()) { // Check if user allowed to transition                  
-        //             event.preventDefault();   // Prevent migration to default state                  
-        //             $state.go('forms');
-        //         }
-        //     }
-        // });
     }
 })();
