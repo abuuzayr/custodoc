5f68d7b52508f0f853fd9947bf4056573f73827a
Dannyzlh <Danielzlh62@gmail.com>
2016-07-28 15:43:37 +0800

    add edit,save for usermgmt

--- client/app/UserMgmt/usermgmt.controller.js
+++ client/app/UserMgmt/usermgmt.controller.js
@@ -11,10 +11,8 @@
         /* =========================================== Initialization =========================================== */
         /* jshint validthis: true */
         var vm = this;
-        var addUserId = 0;
         var MIN_PASSWORD_LENGTH = 8;
         var MAX_PASSWORD_LENGTH = 24;
-        vm.users = [];
         vm.userGroups = ["Admin", "User+", "User"];
 
         vm.tableOptions = {};
@@ -23,7 +21,7 @@
         vm.tableOptions.eanblePagination = true;
         getUsers();
         vm.tableOptions.columnDefs = [
-            { type: 'action', icon: 'edit', action: goEditUser },
+            { type: 'action', icon: 'edit', action: editUser() },
             { type: 'default', displayName: 'Username', fieldName: 'username' },
             { type: 'default', displayName: 'Email', fieldName: 'email' },
             { type: 'default', displayName: 'Usertype', fieldName: 'usertype' }
@@ -32,10 +30,77 @@
 
         vm.openDialog = openDialog;
         vm.closeDialog = closeDialog;
+        vm.editUser = editUser;
+        vm.addUser = addUser;
+        vm.discardEdit = discardEdit;
+        vm.discardAdd = discardAdd;
 
-        function goEditUser() {
-            console.log('goEditUser') //TOFIX
+        function editUser(user) {
+            vm.userInEdit = user;
+            openDialog('editUser-Dialog');
         }
+
+        function SaveEdit(user) {
+            var userData = constructUserData(user);
+            return createUser(userData)
+                .then(SuccessCallback)
+                .catch(ErrorCallback);
+
+            function SuccessCallback(res) {
+                feedbackServices.successFeedback('User created', 'usermgmt-feedbackMessage', 2000);
+                discardEdit();
+            }
+
+            function ErrorCallback(err) {
+                feedbackServices.errorFeedback(err.data, 'usermgmt-feedbackMessage', 2000);
+            }
+        }
+
+        function discardEdit() {
+            vm.userInEdit = null;
+            closeDialog('editUser-Dialog');
+        }
+
+        function addUser() {
+            vm.newUser = {
+                username: '',
+                email: '',
+                password: '',
+                usertype: ''
+            };
+            openDialog('addUser-Dialog');
+        }
+
+        function saveAdd(user) {
+            var userData = constructUserData(user);
+            return updateUser(userData)
+                .then(SuccessCallback)
+                .catch(ErrorCallback);
+
+            function SuccessCallback(res) {
+                discardAdd();
+                feedbackServices.successFeedback('User created', 'usermgmt-feedbackMessage', 2000);
+            }
+
+            function ErrorCallback(err) {
+                feedbackServices.errorFeedback(err.data, 'usermgmt-feedbackMessage', 2000);
+            }
+        }
+
+        function discardAdd() {
+            closeDialog('addUser-Dialog');
+            vm.newUser = null;
+        }
+
+        /**
+         * Converts user information in array to json form to be stored in database. 
+         * 
+         * @returns {json} newUserData
+         */
+        function constructUserData(data) {
+            return data;
+        }
+
 
         /**
          * Get users from database and load into vm.tableOptions.data
@@ -44,76 +109,15 @@
             return usermgmtServices.getUsers().then(SuccessCallback).catch(ErrorCallback);
 
             function SuccessCallback(res) {
-                vm.tableOptions.data = res.data;
+                var data = res.data;
+                for (var i = 0; i < data.length; i++) {
+                    data.usertype = data.application.bulletform.usertype;
+                }
+                vm.tableOptions.data = data;
             }
 
             function ErrorCallback(err) {
                 feedbackServices.errorFeedback(err.data, 'usermgmt-feedbackMessage');
-            }
-        }
-
-
-
-        /* =========================================== Add/Edit user =========================================== */
-        /**
-         * Adds user to database and vm.users if form submission is valid. Returns display error feedback message otherwise.
-         */
-        function addUser() {
-            if ($scope.newUserForm.$valid) {
-                pushElement();
-                clearFormInputs();
-                createInDatabase();
-                closeDialog('userDialog');
-            } else {
-                if (vm.password.length <= MIN_PASSWORD_LENGTH && vm.password.length >= MAX_PASSWORD_LENGTH) {
-                    errMsg = 'Password length should be 8-24 characters';
-                } else {
-                    errMsg = 'Invalid form submission';
-                }
-                return feedbackServices.errorFeedback(errMsg, 'usermgmt-feedbackMessage');
-            }
-        }
-
-        /**
-         * Pushes a new user element into vm.users.
-         */
-        function pushElement() {
-            vm.users[addUserId] = {
-                id: addUserId,
-                username: vm.username,
-                password: vm.password,
-                email: vm.email,
-                selectedUserType: vm.selectedUserType,
-            };
-            addUserId++;
-        }
-
-        /**
-         * Resets all fields in form. Triggered after every form submission. 
-         */
-        function clearFormInputs() {
-            vm.username = '';
-            vm.password = '';
-            vm.email = '';
-            vm.selectedUserType = '';
-        }
-
-        /**
-         * Edits user corresponding to vm.editId and update database if form is valid. Returns display error message otherwise.
-         */
-        function editUser() {
-            if ($scope.newUserForm.$valid) {
-                vm.users[vm.editId].username = vm.username;
-                vm.users[vm.editId].password = vm.password;
-                vm.users[vm.editId].email = vm.email;
-                vm.users[vm.editId].selectedUserType = vm.selectedUserType;
-                // updateDatabase(userId);
-                clearFormInputs();
-                feedbackServices.successFeedback('Users edited successfully', 'usermgmt-feedbackMessage');
-                closeDialog('userDialog');
-            } else {
-                console.log('edituser PHAIL');
-                return feedbackServices.errorFeedback('Invalid form submission', 'usermgmt-feedbackMessage');
             }
         }
 
@@ -134,10 +138,12 @@
 
             function SuccessCallback(res) {
                 deferred.resolve();
+                return feedbackServices.successFeedback('User Deleted', 'usermgmt-feedbackMessage', 2000);
             }
 
             function ErrorCallback(err) {
                 deferred.reject(err);
+                return feedbackServices.successFeedback(err.data, 'usermgmt-feedbackMessage', 2000);
             }
         }
 
@@ -163,12 +169,12 @@
                     }
                 }
                 deferred.resolve();
-                return feedbackServices.successFeedback('User Deleted', 'usermgmt-feedbackMessage', 2000);
+
             }
 
             function ErrorCallback(err) {
                 deferred.reject(err);
-                return feedbackServices.successFeedback(err.data, 'usermgmt-feedbackMessage', 2000);
+
             }
         }
 
@@ -178,36 +184,43 @@
          * @param {object} userData
          */
         function createUser(userData) {
-            return usermgmtServices.createUser(userData)
+            var deferred = $q.defer();
+            usermgmtServices.createUser(userData)
                 .then(SuccessCallback)
                 .catch(ErrorCallback);
+            return deferred.promise;
 
             function SuccessCallback(res) {
-                return feedbackServices.successFeedback('User created', 'usermgmt-feedbackMessage', 2000).then(delayGoState(3000));
+                deferred.resolve();
             }
 
             function ErrorCallback(err) {
-                return feedbackServices.errorFeedback(err.data, 'usermgmt-feedbackMessage');
+                deferred.reject(err);
             }
         }
 
 
         /**
-         * Loads information of user to-be-edited into the form. Triggered when editDialog opens.
+         * Update one user on database. 
          * 
-         * @param {any} username
+         * @param {object} userData
          */
-        function loadEditInfo(username) {
-            for (var i = 0; i < vm.users.length; i++) {
-                if (vm.users[i].username === username) {
-                    vm.editId = i;
-                    vm.username = vm.users[i].username;
-                    vm.password = vm.users[i].password;
-                    vm.email = vm.users[i].email;
-                    vm.selectedUserType = vm.users[i].selectedUserType;
-                }
+        function updateUser(userData) {
+            var deferred = $q.defer();
+            usermgmtServices.update(userData)
+                .then(SuccessCallback)
+                .catch(ErrorCallback);
+            return deferred.promise;
+
+            function SuccessCallback(res) {
+                deferred.resolve();
+            }
+
+            function ErrorCallback(err) {
+                deferred.reject(err);
             }
         }
+
 
         /* =========================================== Dialog =========================================== */
         /**
@@ -216,7 +229,7 @@
          * @param {string} dialogName
          */
         function openDialog(dialogName) {
-            dialogServices.openDialog('usermgmtDialog');
+            dialogServices.openDialog(dialogName);
         }
 
         /**
@@ -225,25 +238,10 @@
          * @param {string} dialogName
          */
         function closeDialog(dialogName) {
-            dialogServices.closeDialog('usermgmtDialog');
+            dialogServices.closeDialog(dialogName);
         }
 
-        /**
-         * Converts user information in array to json form to be stored in database. 
-         * 
-         * @returns {json} newUserData
-         */
-        function convertUserData() {
-            var newUserData = {};
-            newUserData.companyId = companyId;
-            newUserData.companyName = companyName;
-            newUserData.email = vm.users[addUserId].email;
-            newUserData.username = vm.users[addUserId].username;
-            newUserData.password = vm.users[addUserId].password;
-            newUserData.bulletform = {};
-            newUserData.bulletform.usertype = vm.users[addUserId].selectedUserType;
-            return newUserData;
-        }
+
 
         /* =========================================== API =========================================== */
 
