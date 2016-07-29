ae8ab1ba58ccb46f371208caf6a09fe14f0045de
Gilbert <chuakianchiu@gmail.com>
2016-07-29 16:45:28 +0800

    add in break in for loop, break when all forms have been processed

--- client/app/EntryMgmt/entryMgmt.controller.js
+++ client/app/EntryMgmt/entryMgmt.controller.js
@@ -145,6 +145,10 @@ angular.module('app.entryMgmt')
 		            for (var c = 1; c <= vm.numberOfForms; c++) {
 			            for (var i = 1; i <= vm.numberOfPages; i++) { 
 			                p = p.then(generateFormTask);
+			                if (p === vm.numberOfForms) { // already looped all the forms in generateFormTask, no need to stay in the loop anymore
+			                	p = p.then(generateImageTask);
+			                	break;
+			                }
 			                p = p.then(generateImageTask);			            
 			            }
 			            if (i === vm.numberOfPages) {
@@ -397,7 +401,7 @@ angular.module('app.entryMgmt')
 						$compile(node)($scope);
 					}				
 				}		
-				deferred.resolve(formNumber);
+				deferred.resolve(k);
 			});
 			return deferred.promise;
 		}
