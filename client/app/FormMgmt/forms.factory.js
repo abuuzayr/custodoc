137992346008f93baa8975503b76bd534eab7825
Dannyzlh <Danielzlh62@gmail.com>
2016-08-01 15:22:06 +0800

    seperate form routes api call from controller to factory

--- client/app/FormMgmt/forms.factory.js
+++ client/app/FormMgmt/forms.factory.js
@@ -11,21 +11,29 @@
         var service = {
             getNewPage: getNewPage,
             getGroups: getGroups,
-            getForms: getForms,
             createGroup: createGroup,
             deleteGroup: deleteGroup,
-            updateGroup: updateGroup
+            updateGroup: updateGroup,
+            getForms: getForms,
+            deleteForm: deleteForm,
+            updateForm: updateForm,
+            createForm: createForm,
+            getOneForm: getOneForm,
+            formsFactory: formsFactory
         };
 
+        function formsFactory() {
+            var whiteDiv = document.createElement("div");
+            whiteDiv.style.backgroundColor = "white";
+            whiteDiv.style.width = "794px";
+            whiteDiv.style.height = "1123px";
+            whiteDiv.style.zIndex = "-1";
+            whiteDiv.style.position = "relative";
+            whiteDiv.style.top = "-7px";
+            whiteDiv.style.left = "-7px";
+        }
+
         function getNewPage() {
-            var whiteDiv=document.createElement("div");
-            whiteDiv.style.backgroundColor="white";
-            whiteDiv.style.width="794px";
-            whiteDiv.style.height="1123px";
-            whiteDiv.style.zIndex="-1";
-            whiteDiv.style.position="relative";
-            whiteDiv.style.top="-7px";
-            whiteDiv.style.left="-7px";
             var newPage = document.createElement("div");
             newPage.appendChild(whiteDiv.cloneNode(true));
             newPage.style.width = "794px";
@@ -37,36 +45,46 @@
         function getGroups() {
             return $http.get(appConfig.API_URL + "/protected/groups");
         }
-        
+
         function createGroup(groupName) {
             return $http.post(appConfig.API_URL + "/protected/groups", {
-                groupName:groupName
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
+                groupName: groupName
             });
         }
 
-        function deleteGroup(groupName){
-        	return $http.delete(appConfig.API_URL + "/protected/groups/" + groupName);
+        function deleteGroup(groupId) {
+            return $http.delete(appConfig.API_URL + "/protected/groups/" + groupId);
         }
 
-        function updateGroup(groupName, groupData){
-        	$http.put(appConfig.API_URL + "/protected/groups/" + groupName, {
-        			groupData: groupData
-                }, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
+        function updateGroup(groupName, groupData) {
+            $http.put(appConfig.API_URL + "/protected/groups/" + groupName, {
+                groupData: groupData
+            });
         }
 
         function getForms() {
-
+            return $http.get(appConfig.API_URL + "/protected/forms/");
         }
 
+        function getOneForm(formId){
+            return $http.get(appConfig.API_URL + "/protected/forms/" + formId);
+        }
 
+        function deleteForm(formId) {
+            return $http.delete(appConfig.API_URL + "/protected/forms/" + formId);
+        }
+
+        function updateForm(formData) {
+            return $http.put(appConfig.API_URL + "/protected/forms/" + formData._id, {
+                formData: formData
+            });
+        }
+
+        function createForm(formData){
+            return $http.post(appConfig.API_URL + "/protected/forms/" , {
+                formData: formData
+            });
+        }
 
 
         return service;
